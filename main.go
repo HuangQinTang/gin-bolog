49bbf1d6835b5853e8c26499154f7c8d98324bad
HuangQinTang <907236804@qq.com>
2022-10-27 21:23:53 +0800

    文章标签接口

--- main.go
+++ main.go
@@ -1,21 +1,15 @@
 package main
 
 import (
+	"blog/routers"
 	"fmt"
 	"net/http"
-
-	"github.com/gin-gonic/gin"
 
 	"blog/pkg/setting"
 )
 
 func main() {
-	router := gin.Default()
-	router.GET("/test", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "test",
-		})
-	})
+	router := routers.InitRouter()
 
 	s := &http.Server{
 		Addr:           fmt.Sprintf(":%d", setting.HTTPPort),
